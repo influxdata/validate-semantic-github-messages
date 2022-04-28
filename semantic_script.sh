ffb9f2655f3ccc94173b9883bbcf0a91298dea6d
Tyson Kamp <tysonkamp@gmail.com>
2022-04-28 10:38:01 -0700

    feat: handle COMMIT_HISTORY = 0 (ignore commits), #19

--- semantic_script.sh
+++ semantic_script.sh
@@ -8,18 +8,16 @@ else
   echo PR title OK: "$PR_TITLE"
 fi
 
-if (( ${{ inputs.COMMITS_HISTORY}} != 0 )); then
-  while read -r commit; do
-    commit_title=${commit:41}
-    commit_hash_short=${commit:0:7}
+while read -r commit; do
+  commit_title=${commit:41}
+  commit_hash_short=${commit:0:7}
 
-    if [[ ! $commit_title =~ $SEMANTIC_PATTERN ]]; then
-      echo ::error::$commit_hash_short not semantic: "$commit_title"
-      exit_code=1
-    else
-      echo $commit_hash_short OK: "$commit_title"
-    fi
-  done <<< $commits
-fi
+  if [[ ! $commit_title =~ $SEMANTIC_PATTERN ]]; then
+    echo ::error::$commit_hash_short not semantic: "$commit_title"
+    exit_code=1
+  else
+    echo $commit_hash_short OK: "$commit_title"
+  fi
+done <<< $commits
 
 exit $exit_code
