570ce2d8b0eca58b0435fe4883dec2921ebcd494
Tyson Kamp <tysonkamp@gmail.com>
2022-04-29 14:38:39 -0700

    fix: must manually set defaults for inputs when not calling as reusable workflow

--- semantic_script.sh
+++ semantic_script.sh
@@ -1,14 +1,29 @@
 # expected env vars: SEMANTIC_PATTERN, PR_TITLE, COMMITS_URL
 exit_code=0
 
-echo "COMMITS_HISTORY = ${{ inputs.COMMITS_HISTORY }}"
-echo "CHECK_PR_TITLE_OR_ONE_COMMIT = ${{ inputs.CHECK_PR_TITLE_OR_ONE_COMMIT }}"
+# When running the check against its own repo, the inputs will be blank, so
+# in this case set them.  This will never happen when called as a reusable workflow.
+if [[ "" -eq "${{ inputs.COMMITS_HISTORY }}" ]]; then 
+  commits_history=250
+else
+  commits_history=${{ inputs.COMMITS_HISTORY }}
+fi
+
+if [[ "" -eq "${{ inputs.CHECK_PR_TITLE_OR_ONE_COMMIT }}"]]; then
+  tg_style=false
+else
+  tg_style=${{ inputs.CHECK_PR_TITLE_OR_ONE_COMMIT }}
+fi
+  
+
+echo "COMMITS_HISTORY = $commits_history"
+echo "CHECK_PR_TITLE_OR_ONE_COMMIT = $tg_style"
 
 json=$( gh api --paginate $COMMITS_URL )
 commits_count=$(echo $json | jq --raw-output '.[] | [.sha, (.commit.message | split("\n") | first)] | join(" ")' | wc -l)
 check_pr_title=true
 
-if [[ ${{ inputs.CHECK_PR_TITLE_OR_ONE_COMMIT }} == true ]]; then
+if [[ tg_style == true ]]; then
   if (($commits_count == 1 )); then
     check_pr_title=false
     commits_to_check=1
@@ -16,7 +31,7 @@ if [[ ${{ inputs.CHECK_PR_TITLE_OR_ONE_COMMIT }} == true ]]; then
     commits_to_check=0
   fi
 else
-  commits_to_check=${{ inputs.COMMITS_HISTORY }}
+  commits_to_check=$commits_history
 fi
 
 echo "Check pr title: $check_pr_title"
